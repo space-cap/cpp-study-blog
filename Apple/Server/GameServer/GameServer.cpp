17d5065d0c58e0b7b48c5082ada31d1d13ee558f
space-cap <hong.git@yahoo.com>
2024-06-21 17:37:41 +0900

    reserve 사용

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -10,7 +10,7 @@ void Push()
 {
 	for(int32 i=0; i<1000; ++i)
 	{
-		v.push_back(i);
+		v.push_back(1);
 	}
 }
 
@@ -18,15 +18,21 @@ void Push()
 
 int main()
 {
+	v.reserve(20000);
+
 	thread t1(Push);
 	thread t2(Push);
 
 	t1.join();
 	t2.join();
 
+	int32 sum{ 0 };
+	for (auto i : v)
+	{
+		sum += i;
+	}
 
-    
+	cout << sum << endl;
 
-
-    return 0;
+	return 0;
 }
