fd063deaadb9b203b556d88bcaddcd1c573a6797
space-cap <hong.git@yahoo.com>
2024-06-21 15:37:34 +0900

    thread 두번째

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -4,14 +4,14 @@
 #include <thread>
 
 
-void HelloThread()
-{
-	cout << "hello thread" << endl;
+void printMessage(const std::string& message) {
+	std::cout << message << std::endl;
 }
 
 int main()
 {
-    thread t(HelloThread);
+	std::thread t(printMessage, "스레드 1에서 보냅니다.");
+	
 
 	cout << "hello main" << endl;
 
