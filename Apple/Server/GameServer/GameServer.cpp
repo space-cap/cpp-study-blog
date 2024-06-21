2f005474b349a2802cb387f6d0b69f3d3301f1cc
space-cap <hong.git@yahoo.com>
2024-06-21 15:30:54 +0900

    간단한 thread

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -1,8 +1,23 @@
 #include "pch.h"
 #include <iostream>
 #include "CorePch.h"
+#include <thread>
+
+
+void HelloThread()
+{
+	cout << "hello thread" << endl;
+}
 
 int main()
 {
-    HelloWorld();
+    thread t(HelloThread);
+
+	cout << "hello main" << endl;
+
+	if(t.joinable())
+	{
+		t.join();
+	}
+
 }
