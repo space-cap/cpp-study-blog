9c485886940ee8a626190811c4a51697e2788e0f
space-cap <hong.git@yahoo.com>
2024-06-21 15:46:35 +0900

    스레드 분리
    detach()를 사용하여 스레드를 독립적으로 실행

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -4,19 +4,15 @@
 #include <thread>
 
 
+void doWork() {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "작업 완료!" << std::endl;
+}
 
-int main()
-{
-	std::thread t([] {
-		std::cout << "스레드에서 보냅니다." << std::endl;
-		});
-	
-
-	cout << "hello main" << endl;
-
-	if(t.joinable())
-	{
-		t.join();
-	}
-
+int main() {
+    std::thread t(doWork);
+    t.detach();
+    std::cout << "스레드 분리됨." << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    return 0;
 }
