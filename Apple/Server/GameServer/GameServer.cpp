00340dfb35372745309006183ccef611a75cbf2d
space-cap <hong.git@yahoo.com>
2024-06-22 21:52:14 +0900

    SpinLock class 추가

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -5,14 +5,40 @@
 #include <atomic>
 #include <mutex>
 
+
+class SpinLock
+{
+public:
+	void lock()
+	{
+		while (_locked)
+		{
+
+
+		}
+
+		_locked = true;
+	}
+
+	void unlock()
+	{
+		_locked = false;
+	}
+
+private:
+	volatile bool _locked = false;
+};
+
+
 int32 sum = 0;
 mutex m;
+SpinLock spinLock;
 
 void Add()
 {
 	for(int32 i=0; i< 10000; ++i)
 	{
-		lock_guard<mutex> guard(m);
+		lock_guard<SpinLock> guard(spinLock);
 		sum++;
 	}
 }
@@ -21,7 +47,7 @@ void Sub()
 {
 	for (int32 i = 0; i < 10000; ++i)
 	{
-		lock_guard<mutex> guard(m);
+		lock_guard<SpinLock> guard(spinLock);
 		sum--;
 	}
 }
