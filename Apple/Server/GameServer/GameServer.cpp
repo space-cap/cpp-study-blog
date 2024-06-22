0d0a27ae4d7d445e4b13ea4e7efa1627bbbaa035
space-cap <hong.git@yahoo.com>
2024-06-23 01:45:25 +0900

    코드 정리

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -6,69 +6,11 @@
 #include <mutex>
 
 
-class SpinLock
-{
-public:
-	void lock()
-	{
-		// CAS ( Compare-And-Swap)
-		bool expected = false;
-		bool desired = true;
-
-		while(_locked.compare_exchange_strong(expected, desired) == false)
-		{
-			expected = false;
-
-			//this_thread::sleep_for(chrono::milliseconds(100));
-			this_thread::sleep_for(0ms);
-			// this_thread::yield(); // 
-		}
-	}
-
-	void unlock()
-	{
-		//_locked = false;
-		_locked.store(false);
-	}
-
-private:
-	atomic<bool> _locked = false;
-};
-
-
-int32 sum = 0;
-mutex m;
-SpinLock spinLock;
-
-void Add()
-{
-	for(int32 i=0; i< 10000; ++i)
-	{
-		lock_guard<SpinLock> guard(spinLock);
-		sum++;
-	}
-}
-
-void Sub()
-{
-	for (int32 i = 0; i < 10000; ++i)
-	{
-		lock_guard<SpinLock> guard(spinLock);
-		sum--;
-	}
-}
 
 
 int main()
 {
-	std::thread t1(Add);
-	std::thread t2(Sub);
-
-	t1.join();
-	t2.join();
-
-
-	cout << sum << endl;
+	
 
 
 	return 0;
