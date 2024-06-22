01f0219d42e1faf031f2720b6ff61fc9da6d8688
space-cap <hong.git@yahoo.com>
2024-06-22 21:44:06 +0900

    기본 스레드 코드

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -5,8 +5,38 @@
 #include <atomic>
 #include <mutex>
 
+int32 sum = 0;
+mutex m;
+
+void Add()
+{
+	for(int32 i=0; i< 10000; ++i)
+	{
+		lock_guard<mutex> guard(m);
+		sum++;
+	}
+}
+
+void Sub()
+{
+	for (int32 i = 0; i < 10000; ++i)
+	{
+		lock_guard<mutex> guard(m);
+		sum--;
+	}
+}
+
+
 int main()
 {
+	std::thread t1(Add);
+	std::thread t2(Sub);
+
+	t1.join();
+	t2.join();
+
+
+	cout << sum << endl;
 
 
 	return 0;
