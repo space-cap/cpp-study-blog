639873852534ff56f993b1996604420b924f5682
space-cap <hong.git@yahoo.com>
2024-06-23 14:17:18 +0900

    기본 코드
    thread 를 사용하는 방법

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -5,10 +5,32 @@
 #include <atomic>
 #include <mutex>
 #include <windows.h>
+#include <future>
+
+
+int64 Calculate()
+{
+	int64 sum = 0;
+
+	for (int32 i = 0; i < 100; ++i)
+	{
+		sum += i;
+	}
+
+	return sum;
+}
 
 int main()
 {
-	
+	int64 sum = Calculate();
+	cout << sum << endl;
+
+	thread t(Calculate);
+
+	// todo
+
+
+	t.join();
 
 	return 0;
 }
