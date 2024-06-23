1c8fe261798b70a830a3cd00887793021f89a5ef
space-cap <hong.git@yahoo.com>
2024-06-23 14:29:43 +0900

    future 사용해 보기

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -25,12 +25,25 @@ int main()
 	int64 sum = Calculate();
 	cout << sum << endl;
 
-	thread t(Calculate);
+	// std::future
+	{
+		// 1) deferred -> lazy evaluation 지연해서 실행하세요
+		// 2) async -> 별도의 쓰레드를 만들어서 실행하세요
+		// 3) deferred | async -> 둘 중 알아서 골라주세요
+		// 서버가 실행이 될 때 데이터 로딩에서 사용을 해 보는 것도 좋다.
+		std::future<int64> future = std::async(std::launch::async, Calculate);
 
-	// todo
+		// todo
 
+		// 준비가 되었는지 확인하고 싶을 때
+		std::future_status status = future.wait_for(1ms);
+		if(status == future_status::ready)
+		{
+			
+		}
 
-	t.join();
+		int64 sum = future.get();
+	}
 
 	return 0;
 }
