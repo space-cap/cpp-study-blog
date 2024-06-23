6c8831e2d0f6cc4059385d3a0a0664d0846c3131
space-cap <hong.git@yahoo.com>
2024-06-23 15:41:27 +0900

    i j, j i
    시간이 차이가 난다.

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -7,10 +7,45 @@
 #include <windows.h>
 #include <future>
 
+int32 buffer[10000][10000];
 
 
-int main() {
-   
+int main()
+{
+	memset(buffer, 0, sizeof(buffer));
+
+	{
+		uint64 start = GetTickCount64();
+
+		int64 sum = 0;
+		for(int32 i=0; i< 10000; ++i)
+		{
+			for(int32 j=0;j<10000;++j)
+			{
+				sum += buffer[i][j];
+			}
+		}
+
+		uint64 end = GetTickCount64();
+		cout << "Elapsed Tick " << (end - start) << endl;
+	}
+
+
+	{
+		uint64 start = GetTickCount64();
+
+		int64 sum = 0;
+		for (int32 i = 0; i < 10000; ++i)
+		{
+			for (int32 j = 0; j < 10000; ++j)
+			{
+				sum += buffer[j][i];
+			}
+		}
+
+		uint64 end = GetTickCount64();
+		cout << "Elapsed Tick " << (end - start) << endl;
+	}
 
     return 0;
 }
