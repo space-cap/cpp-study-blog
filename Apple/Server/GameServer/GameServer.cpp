74220a721fa5d705e0d800c01d8301890e9dbf33
space-cap <hong.git@yahoo.com>
2024-06-24 18:33:16 +0900

    코드 초기화

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -12,41 +12,7 @@
 
 int main()
 {
-	atomic<bool> flag = false;
-	//flag.is_lock_free();
-
-	//flag = true;
-	flag.store(true);
-
-	//bool val = flag;
-	bool val = flag.load(memory_order_seq_cst);
-
-
-	{
-		bool prev = flag.exchange(true);
-		//bool prev = flag;
-		//flag = true;
-
-
-	}
-
-	// CAS (Compare-And-Swap) 조건부 수정
-	{
-		bool expected = false;
-		bool desired = true;
-		flag.compare_exchange_strong(expected, desired);
-
-		if(flag == expected)
-		{
-			flag = desired;
-			return true;
-		}
-		else
-		{
-			expected = flag;
-			return false;
-		}
-	}
+	
 
 
 	return 0;
