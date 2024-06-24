06756bc1e17611021cc3d073646b9c26771566aa
space-cap <hong.git@yahoo.com>
2024-06-24 18:46:01 +0900

    thread_local 사용법

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -7,15 +7,28 @@
 #include <windows.h>
 #include <future>
 
+//__declspec(thread) int value;
+thread_local int threadLocalVar = 0;
+//thread_local queue<int> q;
 
-
-
-int main()
+void threadFunction(int id)
 {
-	
+    threadLocalVar = id;
 
+    while (true)
+    {
+        cout << "Thread " << id << " has threadLocalVar = " << threadLocalVar << endl;
 
-	return 0;
+        this_thread::sleep_for(1s);
+    }
 }
 
 
+int main() {
+    std::thread t1(threadFunction, 1);
+    std::thread t2(threadFunction, 2);
+    t1.join();
+    t2.join();
+
+	return 0;
+}
