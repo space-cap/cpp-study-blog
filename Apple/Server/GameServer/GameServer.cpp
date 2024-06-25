060ac98a495f5b981630a5c694b40fa010f0e475
space-cap <hong.git@yahoo.com>
2024-06-25 15:34:48 +0900

    코드 초기화

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -6,44 +6,11 @@
 #include <mutex>
 #include <windows.h>
 #include <future>
-#include "ConcurrentQueue.h"
-#include "ConcurrentStack.h"
 
-
-LockFreeQueue<int32> q;
-LockFreeStack<int32> s;
-
-void Push()
-{
-	while (true)
-	{
-		int32 value = rand() % 100;
-		q.Push(value);
-
-		this_thread::sleep_for(1ms);
-	}
-}
-
-void Pop()
-{
-	while (true)
-	{
-		auto data = q.TryPop();
-		if (data != nullptr)
-			cout << (*data) << endl;
-	}
-}
 
 
 int main()
 {
-	thread t1(Push);
-	thread t2(Pop);
-	thread t3(Pop);
-
-	t1.join();
-	t2.join();
-	t3.join();
 
 	return 0;
 }
