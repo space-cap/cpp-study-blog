16dbd03a64b9d64dc441ee6453df4e68de344cdf
space-cap <hong.git@yahoo.com>
2024-06-25 17:01:57 +0900

    ThreadManager 테스트

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -6,12 +6,25 @@
 #include <mutex>
 #include <windows.h>
 #include <future>
+#include "ThreadManager.h"
 
+CoreGlobal Core;
 
+void ThreadMain()
+{
+	while (true)
+	{
+		cout << "Hello ! I am thread... " << LThreadId << endl;
+		this_thread::sleep_for(1s);
+	}
+}
 
 
 int main() {
     
-
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch(ThreadMain);
+	}
     return 0;
 }
