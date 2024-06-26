2a6f35a3e1b89fd13aa1724c675dd81a2d4a463a
space-cap <hong.git@yahoo.com>
2024-06-26 10:26:09 +0900

    코드 초기화

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -12,32 +12,9 @@
 
 
 
-#include "PlayerManager.h"
-#include "AccountManager.h"
-
 int main()
 {
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "PlayerThenAccount" << endl;
-				GPlayerManager.PlayerThenAccount();
-				this_thread::sleep_for(100ms);
-			}
-		});
-
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "AccountThenPlayer" << endl;
-				GAccountManager.AccountThenPlayer();
-				this_thread::sleep_for(100ms);
-			}
-		});
-
-	GThreadManager->Join();
+	
 
     return 0;
 }
