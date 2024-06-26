aa3aba12120ef20b3a9005185dd3b1971052be1a
space-cap <hong.git@yahoo.com>
2024-06-26 15:41:32 +0900

    코드 초기화

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -9,29 +9,12 @@
 #include "ThreadManager.h"
 
 
-#include <iostream>
-#include <memory>
-
-class MyClass {
-public:
-    MyClass() { std::cout << "MyClass Constructor" << std::endl; }
-    ~MyClass() { std::cout << "MyClass Destructor" << std::endl; }
-    void display() { std::cout << "MyClass display" << std::endl; }
-};
 
 int main() {
-    std::shared_ptr<MyClass> ptr1 = std::make_shared<MyClass>();
-    std::weak_ptr<MyClass> weakPtr = ptr1;
 
-    if (auto sharedPtr = weakPtr.lock()) { // weak_ptr에서 shared_ptr 얻기
-        sharedPtr->display();
-        std::cout << "Use count: " << sharedPtr.use_count() << std::endl;
-    }
-    else {
-        std::cout << "Object no longer exists" << std::endl;
-    }
 
-    return 0; // ptr1이 범위를 벗어나면서 MyClass의 소멸자가 호출됩니다.
+
+	return 0;
 }
 
 
