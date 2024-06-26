1f4aa15419f146902bf6b3684af9eab545043bfb
space-cap <hong.git@yahoo.com>
2024-06-26 10:32:30 +0900

    해당 숫자가 소수인지 판단

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -9,13 +9,33 @@
 #include "ThreadManager.h"
 
 
+#include <iostream>
+#include <cmath>
 
+bool isPrime(int number) {
+    if (number <= 1) return false; // 1 이하는 소수가 아님
+    if (number <= 3) return true;  // 2, 3은 소수임
 
+    if (number % 2 == 0 || number % 3 == 0) return false; // 2나 3으로 나눠지는 수는 소수가 아님
 
-int main()
-{
-	
+    for (int i = 5; i * i <= number; i += 6) {
+        if (number % i == 0 || number % (i + 2) == 0) return false;
+    }
+
+    return true;
+}
+
+int main() {
+    int n;
+    std::cout << "Enter a number: ";
+    std::cin >> n;
+
+    if (isPrime(n)) {
+        std::cout << n << " is a prime number." << std::endl;
+    }
+    else {
+        std::cout << n << " is not a prime number." << std::endl;
+    }
 
     return 0;
 }
-
