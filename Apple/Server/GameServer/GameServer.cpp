fdc66b63a5bc186957c86d715baacfa339ea0e6a
space-cap <hong.git@yahoo.com>
2024-06-26 10:42:19 +0900

    소수 구하기
    인터넷에 있는 코드로 확인

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -8,37 +8,49 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include <iostream>
-#include <vector>
 
-int countPrimes(int n) {
-    if (n <= 1) return 0;
 
-    std::vector<bool> isPrime(n + 1, true);
-    isPrime[0] = isPrime[1] = false; // 0과 1은 소수가 아님
+const int N = 100000;
+std::vector<bool> isPrime(N + 1, true);
+std::mutex mtx;
 
-    for (int i = 2; i * i <= n; ++i) {
+void sieveSegment(int start, int end) {
+    for (int i = 2; i * i <= N; ++i) {
         if (isPrime[i]) {
-            for (int j = i * i; j <= n; j += i) {
+            for (int j = max(i * i, (start + i - 1) / i * i); j <= end; j += i) {
+                std::lock_guard<std::mutex> guard(mtx);
                 isPrime[j] = false;
             }
         }
     }
+}
+
+int main() {
+    int numThreads = std::thread::hardware_concurrency(); // 사용 가능한 하드웨어 스레드 수
+    std::vector<std::thread> threads;
+
+    int segmentSize = (N + numThreads - 1) / numThreads;
+
+    isPrime[0] = isPrime[1] = false; // 0과 1은 소수가 아님
+
+    for (int t = 0; t < numThreads; ++t) {
+        int start = t * segmentSize;
+        int end = min((t + 1) * segmentSize - 1, N);
+        threads.emplace_back(sieveSegment, start, end);
+    }
+
+    for (auto& th : threads) {
+        th.join();
+    }
 
     int primeCount = 0;
-    for (int i = 2; i <= n; ++i) {
+    for (int i = 2; i <= N; ++i) {
         if (isPrime[i]) {
             ++primeCount;
         }
     }
 
-    return primeCount;
-}
-
-int main() {
-    int n = 10000;
-    int primeCount = countPrimes(n);
-    std::cout << "Number of primes less than or equal to " << n << " is: " << primeCount << std::endl;
+    std::cout << "Number of primes less than or equal to " << N << " is: " << primeCount << std::endl;
 
     return 0;
 }
