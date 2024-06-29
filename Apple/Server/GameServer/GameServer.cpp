0995258c30949d959624df0933c39f94a7d04a45
space-cap <hong.git@yahoo.com>
2024-06-29 13:51:35 +0900

    gameserver socket

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -15,35 +15,83 @@
 #include <iostream>
 #include <type_traits>
 
-template <typename Target, typename Source>
-constexpr Target dynamic_cast_checked(Source* src) {
-    static_assert(std::is_base_of_v<Source, std::remove_pointer_t<Target>> || std::is_base_of_v<std::remove_pointer_t<Target>, Source>, "Types must be related");
-    return dynamic_cast<Target>(src);
-}
 
-class Base {
-public:
-    virtual ~Base() = default;
-};
-class Derived : public Base {};
-class Unrelated {};
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
-int main() {
-    Base* base = new Base();
-    Derived* derived = new Derived();
-    Unrelated* unrelated = new Unrelated();
+int main()
+{
+	// 윈속 초기화 (ws2_32 라이브러리 초기화)
+	// 관련 정보가 wsaData에 채워짐
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
 
-    // 올바른 캐스트
-    Base* b1 = dynamic_cast_checked<Base*>(derived); // 성공
-    Derived* d1 = dynamic_cast_checked<Derived*>(base); // 런타임에 실패할 수 있음
+	// ad : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
+	// type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
+	// protocol : 0
+	// return : descriptor
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
+	}
 
-    // 잘못된 캐스트 (주석 처리됨)
-    // Unrelated* u = dynamic_cast_checked<Unrelated*>(base); // 컴파일 오류: 관련 없는 타입
+	// 나의 주소는? (IP주소 + Port)->XX 아파트 YY 호
+	SOCKADDR_IN serverAddr; // IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY); //< 니가 알아서 해줘
+	serverAddr.sin_port = ::htons(7777); // 80 : HTTP
 
-    delete base;
-    delete derived;
-    delete unrelated;
+	// 안내원 폰 개통! 식당의 대표 번호
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Bind ErrorCode : " << errCode << endl;
+		return 0;
+	}
 
-    return 0;
+	// 영업 시작!
+	if (::listen(listenSocket, 10) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Listen ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// -----------------------------
+
+	while (true)
+	{
+		SOCKADDR_IN clientAddr; // IPv4
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int32 addrLen = sizeof(clientAddr);
+
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Accept ErrorCode : " << errCode << endl;
+			return 0;
+		}
+
+		// 손님 입장!
+		char ipAddress[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+		cout << "Client Connected! IP = " << ipAddress << endl;
+
+		// TODO
+	}
+
+	// -----------------------------
+
+
+	// 윈속 종료
+	::WSACleanup();
 }
 
