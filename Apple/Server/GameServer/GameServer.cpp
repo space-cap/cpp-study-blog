73492feb3716c325a045f80f079b30029c689d48
space-cap <hong.git@yahoo.com>
2024-06-30 14:48:21 +0900

    udp

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -21,19 +21,71 @@
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
+
+void HandleError(const char* cause)
+{
+	int32 errCode = ::WSAGetLastError();
+	cout << cause << " ErrorCode : " << errCode << endl;
+}
+
 int main()
 {
-	// 윈속 초기화 (ws2_32 라이브러리 초기화)
-	// 관련 정보가 wsaData에 채워짐
 	WSAData wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
 
+	SOCKET serverSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+	if (serverSocket == INVALID_SOCKET)
+	{
+		HandleError("Socket");
+		return 0;
+	}
 
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+	serverAddr.sin_port = ::htons(7777);
 
+	if (::bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		HandleError("Bind");
+		return 0;
+	}
 
+	while (true)
+	{
+		SOCKADDR_IN clientAddr;
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int32 addrLen = sizeof(clientAddr);
 
+		this_thread::sleep_for(1s);
 
+		char recvBuffer[1000];
+
+		int32 recvLen = ::recvfrom(serverSocket, recvBuffer, sizeof(recvBuffer), 0,
+			(SOCKADDR*)&clientAddr, &addrLen);
+
+		if (recvLen <= 0)
+		{
+			HandleError("RecvFrom");
+			return 0;
+		}
+
+		cout << "Recv Data! Data = " << recvBuffer << endl;
+		cout << "Recv Data! Len = " << recvLen << endl;
+
+		int32 errorCode = ::sendto(serverSocket, recvBuffer, recvLen, 0,
+			(SOCKADDR*)&clientAddr, sizeof(clientAddr));
+
+		if (errorCode == SOCKET_ERROR)
+		{
+			HandleError("SendTo");
+			return 0;
+		}
+
+		cout << "Send Data! Len = " << recvLen << endl;
+	}
 
 	// 윈속 종료
 	::WSACleanup();
