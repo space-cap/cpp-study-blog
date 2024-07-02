24482eae131364325c7af0fb8c784f97aa3ace1f
space-cap <hong.git@yahoo.com>
2024-07-02 12:34:35 +0900

    ServerCore 네트워크 관련 코드 추가

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -1,25 +1,14 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
-#include <thread>
 #include <atomic>
 #include <mutex>
 #include <windows.h>
 #include <future>
 #include "ThreadManager.h"
 
-#include "RefCounting.h"
-#include "Memory.h"
-#include "Allocator.h"
-#include "TypeCast.h"
-#include <iostream>
-#include <type_traits>
 
 
-#include <winsock2.h>
-#include <mswsock.h>
-#include <ws2tcpip.h>
-#pragma comment(lib, "ws2_32.lib")
 
 
 
