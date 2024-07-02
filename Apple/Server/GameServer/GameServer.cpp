851b6f932a7f6b68adf2c16fd379e6479956ccec
space-cap <hong.git@yahoo.com>
2024-07-02 14:34:49 +0900

    IocpCore 추가

--- Apple/Server/GameServer/GameServer.cpp
+++ Apple/Server/GameServer/GameServer.cpp
@@ -9,16 +9,24 @@
 
 
 
-
-
-
-
-
-
-
+#include "SocketUtils.h"
 
 int main()
 {
-	
-}
+	SOCKET socket = SocketUtils::CreateSocket();
 
+	SocketUtils::BindAnyAddress(socket, 7777);
+
+	SocketUtils::Listen(socket);
+
+	SOCKET clientSocket = ::accept(socket, nullptr, nullptr);
+
+	cout << "Client Connected!" << endl;
+
+	while (true)
+	{
+
+	}
+
+	GThreadManager->Join();
+}
