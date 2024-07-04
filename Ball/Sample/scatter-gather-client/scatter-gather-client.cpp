5f93c62eeb447efd20f5d70ccda134b8d248a57a
space-cap <hong.git@yahoo.com>
2024-07-04 16:05:46 +0900

    scatter-gather

---
+++ Ball/Sample/scatter-gather-client/scatter-gather-client.cpp
@@ -0,0 +1,72 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <WinSock2.h>
+#include <Mswsock.h>
+#include <iostream>
+#include <thread>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+void CALLBACK CompletionRoutine(DWORD dwError, DWORD cbTransferred, LPWSAOVERLAPPED lpOverlapped, DWORD dwFlags) {
+    std::cout << "WSASend completed with error: " << dwError << " and bytes transferred: " << cbTransferred << std::endl;
+}
+
+int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed." << std::endl;
+        return 1;
+    }
+
+    SOCKET s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (s == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed with error: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(12345); // 서버 포트
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // 서버 IP
+
+    if (connect(s, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Connection failed with error: " << WSAGetLastError() << std::endl;
+        closesocket(s);
+        WSACleanup();
+        return 1;
+    }
+
+    char sendData1[] = "Hello, ";
+    char sendData2[] = "World!";
+    WSABUF wsaBuf[2];
+    wsaBuf[0].buf = sendData1;
+    wsaBuf[0].len = sizeof(sendData1) - 1; // 널 문자를 제외한 길이
+    wsaBuf[1].buf = sendData2;
+    wsaBuf[1].len = sizeof(sendData2) - 1; // 널 문자를 제외한 길이
+
+    DWORD bytesSent = 0;
+    WSAOVERLAPPED overlapped = { 0 };
+
+    while (true) {
+        if (WSASend(s, wsaBuf, 2, &bytesSent, 0, &overlapped, CompletionRoutine) == SOCKET_ERROR) {
+            if (WSAGetLastError() != WSA_IO_PENDING) {
+                std::cerr << "WSASend failed with error: " << WSAGetLastError() << std::endl;
+                closesocket(s);
+                WSACleanup();
+                return 1;
+            }
+        }
+
+        // 비동기 작업이 완료될 때까지 대기
+        SleepEx(INFINITE, TRUE);
+
+        // 1초 대기
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    closesocket(s);
+    WSACleanup();
+
+    return 0;
+}
---
+++ Ball/Sample/scatter-gather-server/scatter-gather-server.cpp
@@ -0,0 +1,85 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <WinSock2.h>
+#include <iostream>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed." << std::endl;
+        return 1;
+    }
+
+    SOCKET listeningSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (listeningSocket == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed with error: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(12345); // 서버 포트
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(listeningSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Bind failed with error: " << WSAGetLastError() << std::endl;
+        closesocket(listeningSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(listeningSocket, SOMAXCONN) == SOCKET_ERROR) {
+        std::cerr << "Listen failed with error: " << WSAGetLastError() << std::endl;
+        closesocket(listeningSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Waiting for connections..." << std::endl;
+
+    SOCKET clientSocket = accept(listeningSocket, NULL, NULL);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Accept failed with error: " << WSAGetLastError() << std::endl;
+        closesocket(listeningSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Client connected." << std::endl;
+
+    char recvBuf1[256];
+    char recvBuf2[256];
+    WSABUF wsaBuf[2];
+    wsaBuf[0].buf = recvBuf1;
+    wsaBuf[0].len = sizeof(recvBuf1);
+    wsaBuf[1].buf = recvBuf2;
+    wsaBuf[1].len = sizeof(recvBuf2);
+
+    DWORD bytesReceived;
+    WSAOVERLAPPED overlapped = { 0 };
+
+    if (WSARecv(clientSocket, wsaBuf, 2, &bytesReceived, 0, &overlapped, NULL) == SOCKET_ERROR) {
+        if (WSAGetLastError() != WSA_IO_PENDING) {
+            std::cerr << "WSARecv failed with error: " << WSAGetLastError() << std::endl;
+            closesocket(clientSocket);
+            closesocket(listeningSocket);
+            WSACleanup();
+            return 1;
+        }
+    }
+
+    // 비동기 작업이 완료될 때까지 대기
+    SleepEx(INFINITE, TRUE);
+
+    std::cout << "Received data in buffer 1: " << std::string(recvBuf1, 0, bytesReceived) << std::endl;
+    std::cout << "Received data in buffer 2: " << std::string(recvBuf2, 0, bytesReceived) << std::endl;
+
+    closesocket(clientSocket);
+    closesocket(listeningSocket);
+    WSACleanup();
+
+    return 0;
+}
