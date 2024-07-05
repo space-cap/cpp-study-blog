ff979b2e32fcf2e415f5752e191b604e84f789d3
space-cap <hong.git@yahoo.com>
2024-07-05 16:16:58 +0900

    protobuf 사용 할 수 있게 수정

--- Apple/Server/GameServer/pch.h
+++ Apple/Server/GameServer/pch.h
@@ -4,8 +4,10 @@
 
 #ifdef _DEBUG
 #pragma comment(lib, "ServerCore\\Debug\\ServerCore.lib")
+#pragma comment(lib, "Protobuf\\Debug\\libprotobufd.lib")
 #else
 #pragma comment(lib, "ServerCore\\Release\\ServerCore.lib")
+#pragma comment(lib, "Protobuf\\Release\\libprotobuf.lib")
 #endif
 
 #include "CorePch.h"
