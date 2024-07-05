2fe6cc96d0b87c87ae44c4b291299f467237d19e
space-cap <hong.git@yahoo.com>
2024-07-05 20:49:02 +0900

    툴 테스트

--- Apple/Server/Tools/PacketGenerator/PacketGenerator.py
+++ Apple/Server/Tools/PacketGenerator/PacketGenerator.py
@@ -5,7 +5,7 @@ import ProtoParser
 def main():
 
 	arg_parser = argparse.ArgumentParser(description = 'PacketGenerator')
-	arg_parser.add_argument('--path', type=str, default='C:/workdir/github-space-cap/cpp-study-blog/Apple/Server/Common\protoc-21.12/bin/Protocol.proto', help='proto path')
+	arg_parser.add_argument('--path', type=str, default='C:/workdir/github-space-cap/cpp-study-blog/Apple/Server/Common/protoc-21.12/bin/Protocol.proto', help='proto path')
 	arg_parser.add_argument('--output', type=str, default='TestPacketHandler', help='output file')
 	arg_parser.add_argument('--recv', type=str, default='C_', help='recv convention')
 	arg_parser.add_argument('--send', type=str, default='S_', help='send convention')
@@ -13,17 +13,17 @@ def main():
 
 	parser = ProtoParser.ProtoParser(1000, args.recv, args.send)
 	parser.parse_proto(args.path)
-	file_loader = jinja2.FileSystemLoader('Templates')
-	env = jinja2.Environment(loader=file_loader)
+	# file_loader = jinja2.FileSystemLoader('Templates')
+	# env = jinja2.Environment(loader=file_loader)
 
-	template = env.get_template('PacketHandler.h')
-	output = template.render(parser=parser, output=args.output)
+	# template = env.get_template('PacketHandler.h')
+	# output = template.render(parser=parser, output=args.output)
 
-	f = open(args.output+'.h', 'w+')
-	f.write(output)
-	f.close()
+	# f = open(args.output+'.h', 'w+')
+	# f.write(output)
+	# f.close()
 
-	print(output)
+	# print(output)
 	return
 
 if __name__ == '__main__':
