25cacf2242d036e1e8d17cc18502e3c096930bfe
space-cap <hong.git@yahoo.com>
2024-07-09 16:09:21 +0900

    jobqueue 수정

--- Apple/Server/ServerCore/JobQueue.h
+++ Apple/Server/ServerCore/JobQueue.h
@@ -1,5 +1,34 @@
 #pragma once
-class JobQueue
+#include "Job.h"
+#include "LockQueue.h"
+
+/*---------------
+	JobQueue
+----------------*/
+
+class JobQueue : public enable_shared_from_this<JobQueue>
 {
+public:
+	void DoAsync(CallbackType&& callback)
+	{
+		Push(ObjectPool<Job>::MakeShared(std::move(callback)));
+	}
+
+	template<typename T, typename Ret, typename... Args>
+	void DoAsync(Ret(T::*memFunc)(Args...), Args... args)
+	{
+		shared_ptr<T> owner = static_pointer_cast<T>(shared_from_this());
+		Push(ObjectPool<Job>::MakeShared(owner, memFunc, std::forward<Args>(args)...));
+	}
+
+	void				ClearJobs() { _jobs.Clear(); }
+
+private:
+	void				Push(JobRef&& job);
+	void				Execute();
+
+protected:
+	LockQueue<JobRef>	_jobs;
+	Atomic<int32>		_jobCount = 0;
 };
 
