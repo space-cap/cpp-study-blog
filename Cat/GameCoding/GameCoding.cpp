e4a635d9ac508b8a53ede0d9dd9efe43a70f7f60
COCO <hong.git@yahoo.com>
2024-08-07 16:03:54 +0900

    pch 추가

--- Cat/GameCoding/GameCoding.cpp
+++ Cat/GameCoding/GameCoding.cpp
@@ -1,6 +1,4 @@
-﻿// GameCoding.cpp : Defines the entry point for the application.
-//
-
+﻿#include "pch.h"
 #include "framework.h"
 #include "GameCoding.h"
 
---
+++ Cat/GameCoding/pch.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <windows.h>
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
+using namespace std;
+
