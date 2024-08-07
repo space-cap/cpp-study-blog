4777651c99c631c551aa60cf2aae8f1585ccd014
COCO <hong.git@yahoo.com>
2024-08-07 17:14:38 +0900

    input manager 추가

--- Cat/GameCoding/Game.cpp
+++ Cat/GameCoding/Game.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "Game.h"
 
+#include "InputManager.h"
 #include "TimeManager.h"
 
 Game::Game()
@@ -17,17 +18,26 @@ void Game::Init(HWND hwnd)
 	_hdc = ::GetDC(hwnd);
 
 	GET_SINGLE(TimeManager)->Init();
+	GET_SINGLE(InputManager)->Init(_hwnd);
+
 }
 
 void Game::Update()
 {
 	GET_SINGLE(TimeManager)->Update();
+	GET_SINGLE(InputManager)->Update();
 }
 
 void Game::Render()
 {
 	uint32 fps = GET_SINGLE(TimeManager)->GetFps();
 	float deltaTime = GET_SINGLE(TimeManager)->GetDeltaTime();
+
+	{
+		POINT mousePos = GET_SINGLE(InputManager)->GetMousePos();
+		wstring str = std::format(L"Mouse({0}, {1})", mousePos.x, mousePos.y);
+		::TextOut(_hdc, 20, 10, str.c_str(), static_cast<int32>(str.size()));
+	}
 
 	{
 		wstring str = std::format(L"FPS({0}), DT({1} ms)", fps, static_cast<int32>(deltaTime * 1000));
